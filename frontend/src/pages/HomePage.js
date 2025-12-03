3a3c7625080400965269e09b04cb9231ad77baba
Szymonppk <szymon.palka@student.pk.edu.pl>
2025-12-03 19:46:35 +0100

    feat(components): added homepage and its components

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import NavBar from '../components/layout/NavBar';
+import HomeSection from '../components/layout/HomeSection';
+
+function HomePage(){
+    
+    return (
+        <>
+            <NavBar/>
+            <HomeSection/>
+        </>
+    );
+}
